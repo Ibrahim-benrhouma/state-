365d7181a37537836781ac0785fdd3edf2fc328e
Ibrahim-benrhouma <ibrahimbenrhouma9@gmail.com>
2021-08-14 19:09:32 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react" ;
+import myphoto from './Myphoto.jpg' ;
+class App extends  React.Component{
+  state={
+    person: {
+      name:"Ibrahim",
+      bio:"Ovic",
+      imgSrc : myphoto,
+      profession : "Web developper" 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    },
+    show : false,
+    count : 0
+  };
+  
+  render(){
+    const showPerson=()=>{
+      this.setState(
+        {
+          show : ! this.state.show
+        }
+      );
+      setInterval(()=>{
+        
+        this.setState(
+          {count : this.state.count + 1}
+        )
+      },1000);
+    }
+    return(
+      <div className="App">
+        <button onClick={showPerson}>show person</button>
+          {this.state.show ? (<div className="profile">
+          <p>{this.state.person.name}</p>
+          <p>{this.state.person.bio}</p>
+          
+          <img src={this.state.person.imgSrc} alt={this.state.person.name} style={{ width: 800, height: 500 }} />
+          <p>{this.state.person.profession}</p>
+          <p>interval :{this.state.count}</p>
+
+          </div>) : <div></div>}
+          
+
+      </div>
+    )
+  }
 }
 
 export default App;
